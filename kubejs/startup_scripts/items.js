0871ac824f51ada0a2ac4a290b7c3169e64e2eae
Justmoonboy <130238647+Justmoonboy@users.noreply.github.com>
2025-09-21 11:30:43 -0500

    Add files via upload

---
+++ kubejs/startup_scripts/items.js
@@ -0,0 +1,5 @@
+StartupEvents.registry('item', e => {
+    e.create('crude_tin_sponge').displayName("Crude Sponge Tin")
+    e.create('blister_copper').displayName("Blistered Copper")
+    e.create('electronic_waste').displayName("Electronic Waste")
+})
