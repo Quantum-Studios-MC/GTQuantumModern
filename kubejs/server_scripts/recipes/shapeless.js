5e54fbb8bcf956b6176be8b4658723a454adbb64
Justmoonboy <130238647+Justmoonboy@users.noreply.github.com>
2025-09-21 11:30:09 -0500

    Add files via upload

---
+++ kubejs/server_scripts/recipes/shapeless.js
@@ -0,0 +1,15 @@
+ServerEvents.recipes(event => {
+    event.shapeless('2x minecraft:bone', [
+    'minecraft:skeleton_skull']
+        )
+    event.shapeless('2x ytech:antler', [
+    'bonezone:spinal_skull_deer']
+        )
+    event.shapeless('kubejs:electronic_waste', [
+    'nukacraft:powerblock']
+        )
+    event.shapeless('2x kubejs:electronic_waste', [
+    'nukacraft:comblock']
+        )
+    }
+)
